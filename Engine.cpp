18bfa1a1caaedf140920f85797a783cf7fd0cfc0
Petko Bonev <9mi0800149@g.fmi.uni-sofia.bg>
2024-05-15 11:33:00 +0300

    class Command added and modified Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -1,39 +1,63 @@
 #include <iostream>
 #include <string>
+#include "Command.cpp"
 
 void engine()
 {
-    bool flag = true;
-    while(flag)
+    std::string input;
+    std::cout<<"Enter a command:";
+    getline(std::cin, input);
+    std::string cmd;
+    std::string args;
+    bool inCmd = true;
+    for(int i = 0; i < input.size(); i++)
     {
-        std::cout << "1. Create a new spreadsheet\n";
-        std::cout << "2. Open a spreadsheet\n";
-        std::cout << "3. Exit\n";
-        int choice;
-        std::cin >> choice;
-        switch(choice)
+        if(input[i] == ' ')//if we read a space we are done with the command
         {
-            case 1:
-                std::cout << "Enter the name of the spreadsheet: ";
-                std::string name;
-                std::cin >> name;
-                std::cout << "Enter the number of rows: ";
-                int rows;
-                std::cin >> rows;
-                std::cout << "Enter the number of columns: ";
-                int columns;
-                std::cin >> columns;
-                break;
-            case 2:
-                std::cout << "Enter the name of the spreadsheet: ";
-                std::string name;
-                std::cin >> name;
-                break;
-            case 3:
-                flag = false;
-                break;
-            default:
-                std::cout << "Invalid choice\n";
+            inCmd = false;
+            continue;
+        }
+        if(inCmd)//if we read a command we push to cmd
+        {
+            cmd.push_back(input[i]);
+        }
+        else// else we push to arguments
+        {
+            args.push_back(input[i]);
         }
     }
+
+    //cmd.push_back('\0');
+
+    std::cout <<"Entered Command:"<< cmd << std::endl;
+    std::cout <<"Entered Arguments:"<< args << std::endl;
+
+    if(cmd.compare("hello") == 0)//its not the terminating 0//cmd == "hello"
+    {
+        std::cout<<"Hello world\n";
+        
+    }
+    else if(cmd.compare("plus") == 0)
+    {
+        //command = new Plus(args);
+         
+        std::string firstnum = args.substr(0,1);
+        std::string secondnum = args.substr(1,2);
+        int num1 = std::stoi(firstnum);
+        int num2 = std::stoi(secondnum);
+        std::cout<<"Plus: "<<num1 + num2 <<std::endl;
+        
+
+    }
+    else
+    {
+        std::cout << "Invalid command\n";
+    }
+    //command->execute();
+}
+
+int main()
+{
+    engine();
+    return 0;
 }
